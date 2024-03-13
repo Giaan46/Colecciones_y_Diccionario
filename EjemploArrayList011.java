f9b2c24ece17b1a7b6765a82bae952843af7ec02
Gianluca Maida <124819060+Giaan46@users.noreply.github.com>
2024-03-13 19:04:59 +0100

    Update EjemploArrayList011.java

--- EjemploArrayList011.java
+++ EjemploArrayList011.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 /**
  * Ejemplo de uso de la clase ArrayList
  * 
- * @author Luis José Sánchez
+ * @author Gianluca Maida
  */
 public class EjemploArrayList011 {
   public static void main(String[] args) {
